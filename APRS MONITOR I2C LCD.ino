d9432e823af288001d48389d34a70df30432369a
MOHD ZAKI BIN ABDULLAH <67680163+mzakiab@users.noreply.github.com>
2023-01-21 21:08:13 +0800

    Update APRS MONITOR I2C LCD.ino

--- APRS MONITOR I2C LCD.ino
+++ APRS MONITOR I2C LCD.ino
@@ -11,7 +11,7 @@ Koding asal https://github.com/chokelive/aprs_tnc
 #include<Wire.h>
 int LED_RX= 8; // rx dan proses LED indicator 
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 /*  I2C LCD     ARDUINO (NANO/UNO)
     VCC         5V
