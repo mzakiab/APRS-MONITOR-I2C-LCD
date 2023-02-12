0956d89fa6601a6ddc7f9a2a4d60d8ed6c7133ad
MOHD ZAKI BIN ABDULLAH <67680163+mzakiab@users.noreply.github.com>
2023-02-12 17:31:51 +0800

    Add files via upload

--- APRS MONITOR I2C LCD.ino
+++ APRS MONITOR I2C LCD.ino
@@ -1,15 +1,32 @@
 /*
-Project: Monitor APRS Signal
-By: 9W2KEY
-Hardware: I2C LCD display
-Koding asal https://github.com/chokelive/aprs_tnc 
+Project:        Monitor APRS Signal
+By:             9W2KEY
+Hardware:       I2C 20 x 4 LCD display
+Koding asal     https://github.com/chokelive/aprs_tnc 
+31 Jan, 2023 hingga 3hb Feb,2023 (2am) mintak bantuan 9M2CIO
+
+Terima Kasih abe Din atas bantuan ini..
 */
+
 #include <LibAPRS.h>
 #include <LiquidCrystal_I2C.h>
-#include<Wire.h>
-int LED_RX= 8; // rx dan proses LED indicator 
+#include <Wire.h>
+int LED_RX = 8;             // rx dan proses LED indicator 
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+int infoLen; 
+int i;
+int maxLen;
+
+int columnLeft; 
+int columnWidth; 
+int columnNow;
+
+int rowTop; 
+int rowHeight; 
+int rowNow;
+
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);  // set the LCD address to 0x27 for a 20 chars and 4 line display
 
 /*  I2C LCD     ARDUINO (NANO/UNO)
     VCC         5V
@@ -19,9 +36,10 @@ LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars
 */
 
 // APRS Configulation
-#define CALL_SIGN "9W2KEY" // sila bubuh callsign sendiri
+#define CALL_SIGN "9W2KEY"    // sila bubuh callsign sendiri
 #define CALL_SIGN_SSID 9
 
+// #define ADC_REFERENCE REF_3V3
 #define ADC_REFERENCE REF_5V
 #define OPEN_SQUELCH false
 
@@ -51,8 +69,8 @@ void setup() {
   lcd.print("APRS Decoder LCD");
   lcd.setCursor(4,1);
   lcd.print("by 9W2KEY");
-}
 
+} 
 
 void loop() {
   
@@ -62,9 +80,30 @@ void loop() {
 
 
 void processPacket() {
-  char sentence[150];
+  char sentence; // 
+
+    int rowTop = 1;                     // 2nd line
+    int rowHeight = 2;                  // display height
+    int rowNow;
+
+    int columnLeft = 0;                 // 1st column
+    int columnWidth = 16;               // display width
+    int columnNow;
+
+    int infoLen;
+    int i;
+    boolean done;
+ 
   if (gotPacket) {
     gotPacket = false;
+
+    boolean done = false;
+    infoLen = incomingPacket.len;
+    columnLeft = 0;
+    columnWidth = 16;
+    rowTop = 1;
+    rowHeight = 2;
+
     
     Serial.print(F("Received APRS packet. SRC: "));
     Serial.print(incomingPacket.src.call);
@@ -81,29 +120,70 @@ void processPacket() {
     }
     Serial.println("");
 
-    digitalWrite(LED_RX, HIGH); // ON LED 
-    delay(500); // tunggu kejap nak proses data yg masuk
-    lcd.clear(); // clear display dulu
-    lcd.setCursor(0, 0); 
-    lcd.print(incomingPacket.src.call);
-    lcd.print(F("-"));
-    lcd.print(incomingPacket.src.ssid);
-    lcd.print(F(" >"));
-    lcd.print(incomingPacket.dst.call);
-    //lcd.println();
-    //lcd.set1X();
-    //lcd.println();
-    lcd.setCursor(0, 1);
-    for (int i = 0; i < incomingPacket.len; i++) {
-    if(i%20==0) lcd.println();
-      lcd.write(incomingPacket.info[i]);
-      lcd.blink(); // cursor kelip kelip
-    }
-    // lcd.println("");
+    digitalWrite(LED_RX, HIGH);       // ON LED 
+    delay(500);                       // tunggu kejap nak proses data yg masuk
+    lcd.clear();                      // clear display dulu
+
+lcd.setCursor(0, 0);
+lcd.print(incomingPacket.src.call);
+lcd.print(F("-"));
+lcd.print(incomingPacket.src.ssid);
+lcd.print(F(" "));
+lcd.print(incomingPacket.dst.call);
+// lcd.print(F("-"));
+lcd.print(incomingPacket.dst.ssid);
+
+/*********************** SETUP UNTUK LCD 16 x 2 *********************/
+    rowNow = rowTop;                    // start at top row
+    columnNow = columnLeft;             // and left column
+
+    infoLen = incomingPacket.len;       // max info length
+    i = 0;                              // info string index
+    done = false;                       // display full flag
+
+    lcd.setCursor(columnNow,rowNow);    // start here
+
+    while ( ! done && i < infoLen )     // while not done and still have info
+    {
+//     lcd.setCursor(columnNow,rowNow); // line utk bui jelas, keep comment
+       lcd.write(incomingPacket.info[i]);       // put 1 char
+       i++;                                     // next char
+
+       columnNow++;                             // next column
+       if ( columnNow >= columnWidth )          // if too wide
+       {
+          columnNow = columnLeft;               // to the left, <cr>
+          rowNow++;                             // to the next line, <lf>
+          done = ( rowNow >= rowHeight );       // if max height, done
+          if ( ! done )                         // otherwise
+          {  lcd.setCursor(columnNow,rowNow); } // move the cursor to new line
+       }
+   }
+   
+   while ( ( ! done ) && ( i < infoLen ) )
+   {     // belum habis/done dan ada info belum print
+       if ( columnNow >= columnWidth )  // if columNow dah terkanan
+       {
+          columnNow = columnLeft;       // gi ke kiri, <cr>
+          if ( rowNow >= rowHeight )    // if rowNow dah di bawah
+          { done = true; }              // dah habis dah
+          else { rowNow++; }            // belum habis, gi next line, <lf>
+       }
+       else { columnNow++; }            // move a column to right
+
+       if ( ! done )                    // if belum habis/done
+       {
+          lcd.setCursor(columnNow,rowNow);      // set cursor position
+         lcd.write(incomingPacket.info[i]);    // muntahkannya
+          i++;                                  // next char dlm info
+       }
+   }
+    
+    lcd.blink(); // cursor kelip kelip
     digitalWrite(LED_RX, LOW); // OFF LED
     free(packetData);
 
-    // Serial.print(F("Free RAM: ")); Serial.println(freeMemory());
+   // Serial.print(F("Free RAM: ")); Serial.println(freeMemory());
   }
 }
 
